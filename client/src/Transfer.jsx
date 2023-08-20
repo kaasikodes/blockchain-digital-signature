2b7fa00c7339647a4fdd67e91a5a9391bf75eac4
kaasikodes <thisbroisfresh@gmail.com>
2023-08-20 17:40:20 +0100

    chore: adds creation of msg hash via private key functionality

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,23 +1,35 @@
 import { useState } from "react";
 import server from "./server";
+import { secp256k1 } from "ethereum-cryptography/secp256k1";
+import { sha256 } from "ethereum-cryptography/sha256";
+import { utf8ToBytes, toHex } from "ethereum-cryptography/utils";
+import { serializeSignature } from "./lib/utils";
 
 function Transfer({ address, setBalance }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
 
   const setValue = (setter) => (evt) => setter(evt.target.value);
 
   async function transfer(evt) {
     evt.preventDefault();
+    const amount = parseInt(sendAmount);
+    const msgHash = sha256(utf8ToBytes(JSON.stringify({ amount, recipient })));
+
+    const signature = secp256k1.sign(msgHash, privateKey);
 
     try {
+      const res = await server.post(`send`, {
+        sender: address,
+        amount,
+        recipient,
+        signature: serializeSignature(signature),
+        msgHash: toHex(msgHash),
+      });
       const {
         data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
+      } = res;
       setBalance(balance);
     } catch (ex) {
       alert(ex.response.data.message);
@@ -43,6 +55,14 @@ function Transfer({ address, setBalance }) {
           placeholder="Type an address, for example: 0x2"
           value={recipient}
           onChange={setValue(setRecipient)}
+        ></input>
+      </label>
+      <label>
+        Sign with Private Key
+        <input
+          placeholder="Enter private key to sign transaction"
+          value={privateKey}
+          onChange={setValue(setPrivateKey)}
         ></input>
       </label>
 
